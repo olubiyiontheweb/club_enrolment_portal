aae212557e1fd08b11d9f7858dd3ed4edae9f214
Oluwatosin Olubiyi <29473996+olubiyiontheweb@users.noreply.github.com>
2022-03-05 12:09:41 +0000

    Email sends, database added and authentication done

---
+++ clubEnrolmentPortal_TheKangaroos/Services/AuthEmailSendGridKeyGetter.cs
@@ -0,0 +1,9 @@
+namespace clubEnrolmentPortal_TheKangaroos.Services
+{
+    public class AuthEmailSendGridKeyGetter
+    {
+        // this is a reference to the SendGrid key in the user secrets
+        #nullable enable
+        public string? SendGridKey { get; set; }
+    }
+}
